31a6659c33dfea9cddb07185ecdd5908b59fd26b
twonezero <dnjsdudtm98@naver.com>
2023-12-11 13:56:20 +0900

    #22 -  회원 엔티티와 게시글, 댓글의 연관관계 적용
    * `data.sql` 테스트 데이터도 모두 업데이트 (이전 커밋에서 시행함 - `d38702`)
    
    게시글 -> 댓글 컬렉션의 정렬을 최신 순으로 적용 `createdAt DESC`

--- src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/fastcampus/projectboard/repository/JpaRepositoryTest.java
@@ -2,6 +2,7 @@ package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.config.JpaConfig;
 import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.UserAccount;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,13 +25,16 @@ class JpaRepositoryTest {
 
     private final ArticleRepository articleRepository;
     private final ArticleCommentRepository articleCommentRepository;
+    private final UserAccountRepository userAccountRepository;
 
     public JpaRepositoryTest(
             @Autowired ArticleRepository articleRepository,
-            @Autowired ArticleCommentRepository articleCommentRepository
+            @Autowired ArticleCommentRepository articleCommentRepository,
+            @Autowired UserAccountRepository userAccountRepository
     ) {
         this.articleRepository = articleRepository;
         this.articleCommentRepository = articleCommentRepository;
+        this.userAccountRepository = userAccountRepository;
     }
 
     @DisplayName("select 테스트")
@@ -53,10 +57,13 @@ class JpaRepositoryTest {
     void givenTestData_whenInserting_thenWorksFine() {
         //Given
         long prevCntOfArticles = articleRepository.count();
-        Article article = Article.of("new article", "new content", "#spring");
+        UserAccount userAccount = userAccountRepository.save(
+                UserAccount.of("twonezero", "asdf1234", null, null, null)
+        );
+        Article article = Article.of(userAccount, "new Article", "new content", "#spring");
 
         //When
-        Article savedArticle = articleRepository.save(article);
+        articleRepository.save(article);
 
         //Then
         assertThat(articleRepository.count())

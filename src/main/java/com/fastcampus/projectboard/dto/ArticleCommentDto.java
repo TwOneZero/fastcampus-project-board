19de9b245187cb8246fdda2d4dbd47a77490aafe
twonezero <dnjsdudtm98@naver.com>
2024-02-21 16:26:04 +0900

    #69 - 대댓글 비즈니스 로직 구현
    * 부모/자식 관계를 만들면서 entity <-> dto 간 반환 로직
    * 댓글 서비스에서 댓글과 대댓글을 저장하고 삭제하는 로직
    * **댓글과 대댓글을 db 엔티티로 부터 변환할 때, 서로 계층구조를 만들고 각각 정렬하는 과정을 구현**

--- src/main/java/com/fastcampus/projectboard/dto/ArticleCommentDto.java
+++ src/main/java/com/fastcampus/projectboard/dto/ArticleCommentDto.java
@@ -13,6 +13,8 @@ public record ArticleCommentDto(
         Long id,
         Long articleId,
         UserAccountDto userAccountDto,
+
+        Long parentCommentId,
         String content,
         LocalDateTime createdAt,
         String createdBy,
@@ -21,11 +23,16 @@ public record ArticleCommentDto(
 ) {
 
     public static ArticleCommentDto of(Long articleId, UserAccountDto userAccountDto, String content) {
-        return new ArticleCommentDto(null, articleId, userAccountDto, content,null,null,null,null);
+        return  ArticleCommentDto.of(articleId, userAccountDto,null, content);
     }
 
-    public static ArticleCommentDto of(Long id, Long articleId, UserAccountDto userAccountDto, String content, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
-        return new ArticleCommentDto(id, articleId, userAccountDto, content, createdAt, createdBy, modifiedAt, modifiedBy);
+    public static ArticleCommentDto of(Long articleId, UserAccountDto userAccountDto, Long parentCommentId, String content) {
+        return  ArticleCommentDto.of(null, articleId, userAccountDto,parentCommentId, content,null,null,null,null);
+    }
+
+
+    public static ArticleCommentDto of(Long id, Long articleId, UserAccountDto userAccountDto,Long parentCommentId, String content, LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleCommentDto(id, articleId, userAccountDto, parentCommentId, content, createdAt, createdBy, modifiedAt, modifiedBy);
     }
 
     // Domain 과 dto 를 분리하여 연관관계 맵핑이 dto 에서 이루어질 수 있도록 함 ,  osiv 와도 연관됨 (TODO : osiv 찾아보기)
@@ -34,6 +41,7 @@ public record ArticleCommentDto(
                 entity.getId(),
                 entity.getArticle().getId(),
                 UserAccountDto.from(entity.getUserAccount()),
+                entity.getParentCommentId(),
                 entity.getContent(),
                 entity.getCreatedAt(),
                 entity.getCreatedBy(),

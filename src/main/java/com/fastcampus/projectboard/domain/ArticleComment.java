761fd87d5fb62ad661cc8995e102213fff84ded4
twonezero <dnjsdudtm98@naver.com>
2024-02-21 14:09:42 +0900

    #67 - 대댓글 도메인 설계
    댓글 도메인 안에서 부모, 자식 관계를 설정하는 코드를 추가
    자식 댓글의 컬렉션 변화가 쿼리에 반영되게끔
    cascading 규칙을 모두 적용함.
    단방향 연관관계 설정을 사용하여 부모 댓글을 엔티티가 아닌 `Long ` id 를 직접 표현

--- src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -3,7 +3,9 @@ package com.fastcampus.projectboard.domain;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 
 // @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -31,6 +33,15 @@ public class ArticleComment extends AuditingFields {
     private UserAccount userAccount;
 
     @Setter
+    @Column(updatable = false)
+    private Long parentCommentId; // 부모 댓글 id
+
+    @ToString.Exclude
+    @OrderBy("createdAt ASC")
+    @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL)
+    private Set<ArticleComment> childComments = new LinkedHashSet<>();
+
+    @Setter
     @Column(nullable = false, length = 500)
     private String content; // 내용
 
@@ -38,14 +49,20 @@ public class ArticleComment extends AuditingFields {
     protected ArticleComment() {
     }
 
-    private ArticleComment(Article article, UserAccount userAccount, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, Long parentCommentId, String content) {
         this.article = article;
         this.userAccount = userAccount;
+        this.parentCommentId = parentCommentId;
         this.content = content;
     }
 
     public static ArticleComment of(Article article, UserAccount userAccount, String content) {
-        return new ArticleComment(article, userAccount, content);
+        return new ArticleComment(article, userAccount, null, content);
+    }
+
+    public void addChildComment(ArticleComment child) {
+        child.setParentCommentId(this.getId());
+        this.getChildComments().add(child);
     }
 
     @Override
